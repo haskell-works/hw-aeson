a39a96d46293cda6ff2ebbb9c1d441ae135b6885
John Ky <newhoggy@gmail.com>
2017-06-07 23:18:44 +1000

    Tests and CI

--- test/HaskellWorks/Data/AesonSpec.hs
+++ test/HaskellWorks/Data/AesonSpec.hs
@@ -4,11 +4,23 @@ module HaskellWorks.Data.AesonSpec
   ( spec
   ) where
 
+import Data.Aeson
+import Data.Semigroup
+import HaskellWorks.Data.Aeson
 import Test.Hspec
 
 {-# ANN module ("HLint: ignore Redundant do"  :: String) #-}
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.Aeson" $ do
-    it "xxx" $ do
-      True `shouldBe` True
+  it "123123" $ do
+    let actual = objectWith
+          (  "one"    .=! (1 :: Int)
+          <> "two"    .=? Just (2 :: Int)
+          <> "three"  .=? (Nothing :: Maybe Int)
+          )
+    let expected = object
+          [ "one"    .= (1 :: Int)
+          , "two"    .= (2 :: Int)
+          ]
+    actual `shouldBe` expected
