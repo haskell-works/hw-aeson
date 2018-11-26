32ddf9a1ea9f140a67da975fd54cd058bf066e44
John Ky <newhoggy@gmail.com>
2018-11-26 16:05:01 +1100

    Add readJson function

--- src/HaskellWorks/Data/Aeson.hs
+++ src/HaskellWorks/Data/Aeson.hs
@@ -1,13 +1,20 @@
 module HaskellWorks.Data.Aeson
     ( objectWithoutNulls
+    , readJson
     ) where
 
 import Data.Aeson
 import Data.Aeson.Types
 import Data.Text
+import Text.Read
 
 objectWithoutNulls :: [Pair] -> Value
 objectWithoutNulls = object . Prelude.filter (not . isNull . snd)
   where
     isNull Null = True
     isNull _    = False
+
+readJson :: Read a => String -> String -> Parser a
+readJson t s = case readMaybe s of
+  Just a  -> pure a
+  Nothing -> fail $ "Could not parse " <> t
